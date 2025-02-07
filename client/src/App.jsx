ad753329b034c6bc0d352c3c86f2db62f74389e1
seifmagdy2000 <seifmagdy20000@gmail.com>
2025-02-07 16:43:33 +0200

    added dark mode for Nav component and added toggle for dark and light mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import CreatePage from './pages/CreatePage.jsx'; 
+import Nav from './components/Nav.jsx';
+import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
 
+  const toggleDarkMode = () => {
+    setDarkMode(prevMode => !prevMode);
+  };
 
   return (
-    <>
-    <h1>hiii</h1>
-    </>
-  )
+    <div className={darkMode ? 'dark-mode' : ''}>
+      <Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Nav from '../components/Nav.jsx'
+const HomePage = () => {
+}
+
+export default HomePage
